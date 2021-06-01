138f14a5a9d066ac098867f307a55ef1fc875b12
Luke Hampton <lukehampton6@gmail.com>
2021-05-31 20:35:27 -0400

    save to storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-fetch ()
+var recentSearches = JSON.parse(localStorage.getItem("recentSearches")) || [];
+var inputArea = document.getElementsByClassName("input-area");
+
+document.getElementById("button").addEventListener("click", function () {
+  var input = document.getElementById("input").value;
+  input.trim();
+  recentSearches.push(input);
+  recentSearches.reverse();
+  recentSearches.splice(5);
+
+  localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+});
+
+var createHistory = function () {
+  for (i = 0; i < recentSearches.length; i++) {
+    var newSearch = document.createElement("h4");
+    newSearch.textContent = recentSearches[i];
+    
+  }
+};
